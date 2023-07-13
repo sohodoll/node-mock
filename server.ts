9caf4455964c6389e05d6f0e1a3b639429f363f6
sohodoll <kovalevvtl@gmail.com>
2023-07-13 12:40:13 +0300

    feat: add server api

---
+++ server.ts
@@ -0,0 +1,73 @@
+import express, { Request, Response } from 'express'
+import bodyParser from 'body-parser'
+import fs from 'fs'
+import { AbortController } from 'abort-controller'
+
+const app = express()
+const PORT = 3000
+
+// Middleware
+app.use(bodyParser.json())
+
+// Reference to the ongoing request and its controller
+let currentRequest: Request | null = null
+let currentController: AbortController | null = null
+
+// Routes
+app.post('/search', (req: Request, res: Response) => {
+  const searchEmail: string = req.body.email || ''
+  const searchNumber: string = req.body.number || ''
+
+  // Cancel the previous request if it is still ongoing
+  if (currentController) {
+    currentController.abort()
+    console.log('Previous request canceled')
+  }
+
+  // Create a new AbortController for the current request
+  const controller = new AbortController()
+  const { signal } = controller
+
+  // Store the current request and its controller
+  currentRequest = req
+  currentController = controller
+
+  // Delay the request processing
+  setTimeout(() => {
+    // Check if the request is still active
+    if (currentRequest === req) {
+      fs.readFile('data.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err)
+          currentRequest = null
+          currentController = null
+          return res.status(500).json({ error: 'Internal Server Error' })
+        }
+
+        try {
+          const jsonData: any[] = JSON.parse(data)
+
+          const matchingData = jsonData.filter((entry) => {
+            const emailMatch: boolean = entry.email.toLowerCase().includes(searchEmail.toLowerCase())
+            const numberMatch: boolean = entry.number.includes(searchNumber)
+            return emailMatch && numberMatch
+          })
+
+          currentRequest = null
+          currentController = null
+          res.json(matchingData)
+        } catch (error) {
+          console.error(error)
+          currentRequest = null
+          currentController = null
+          res.status(500).json({ error: 'Internal Server Error' })
+        }
+      })
+    }
+  }, 5000)
+})
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
